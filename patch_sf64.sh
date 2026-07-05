c064ece0e937612ac0224f03f0cde57367b48342
pahihu <pahihu@gmail.com>
2026-07-05 21:39:00 +0200

    Update patch_sf64.sh to include v4.1.18 as comment

--- patch_sf64.sh
+++ patch_sf64.sh
@@ -1,5 +1,5 @@
 
-# patch SwiftForth v4.0.4/v4.0.5/v4.0.6 to run under Rocky Linux 8
+# patch SwiftForth v4.0.4-v4.1.18 to run under Rocky Linux 8
 cd $HOME/SwiftForth/bin/linux
 
 xxd sf64 sf64.xxd
